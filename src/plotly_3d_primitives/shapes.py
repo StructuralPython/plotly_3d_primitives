eb9b66aa4632a8b214af9d054ccb7cb28caf4832
Connor Ferster <connorferster@gmail.com>
2024-10-30 15:11:24 -0700

    ensure all orientable geometries are oriented correctly
    - add apply_transformations
    - general cleanup
    - ruff format

--- src/plotly_3d_primitives/shapes.py
+++ src/plotly_3d_primitives/shapes.py
@@ -37,24 +37,24 @@ def cube(
         x=x_array, y=y_array, z=z_array, opacity=opacity, color=color, alphahull=0
     )
     # mesh = go.Mesh3d(
-    #     x=x_array, 
-    #     y=y_array, 
+    #     x=x_array,
+    #     y=y_array,
     #     z=z_array,
     #     i=i_array,
     #     j=j_array,
     #     k=k_array,
-    #     opacity=opacity, 
+    #     opacity=opacity,
     #     color=color,
     # )
     return mesh
 
 
 def prism(
-    center=(0., 0., 0.),
+    center=(0.0, 0.0, 0.0),
     direction=(1.0, 0.0, 0.0),
     radius=0.5,
     height=1.0,
-    n_sides:int = 4,
+    n_sides: int = 4,
     capping=True,
     color: str = "#aaa",
     opacity: float = 0.5,
@@ -62,19 +62,19 @@ def prism(
     anchor_x, anchor_y, anchor_z = center
 
     arr = np.linspace(0, 2 * np.pi, num=n_sides, endpoint=False)
-    z_poly = np.cos(arr) + anchor_z
-    y_poly = np.sin(arr) + anchor_y
+    z_poly = np.cos(arr) + anchor_z + radius
+    y_poly = np.sin(arr) + anchor_y + radius
     x_poly = np.zeros(n_sides) + anchor_x
 
     x_array = np.concat([x_poly, x_poly + height])
     y_array = np.concat([y_poly, y_poly])
     z_array = np.concat([z_poly, z_poly])
 
-    matrix = build_point_matrix(x_array, y_array, z_array)
+    x_array, y_array, z_array = apply_transformations(x_array, y_array, z_array, center, direction)
 
     return go.Mesh3d(
         x=x_array, y=y_array, z=z_array, alphahull=0, color=color, opacity=opacity
-    ), matrix
+    )
 
 
 def cone(
@@ -83,7 +83,7 @@ def cone(
     height=1.0,
     radius: Optional[float] = None,
     capping: bool = True,
-    angle: Optional[float]=None,
+    angle: Optional[float] = None,
     resolution: int = 6,
     color: str = "#aaa",
     opacity: float = 0.5,
@@ -91,10 +91,10 @@ def cone(
     anchor_x, anchor_y, anchor_z = center
 
     arr = np.linspace(
-        0 + np.radians(angle), 
-        2 * np.pi + np.radians(angle), 
-        num=resolution, 
-        endpoint=False
+        0 + np.radians(angle),
+        2 * np.pi + np.radians(angle),
+        num=resolution,
+        endpoint=False,
     )
     z_poly = np.cos(arr) * radius + anchor_x
     y_poly = np.sin(arr) * radius + anchor_y
@@ -104,17 +104,17 @@ def cone(
     y_array = np.concat([y_poly, np.array([anchor_y])])
     x_array = np.concat([x_poly, np.array([anchor_x + height])])
 
-    matrix = np.array([x_array, y_array, z_array, np.ones(len(x_array))])
+    x_array, y_array, z_array = apply_transformations(x_array, y_array, z_array, center, direction)
 
     return go.Mesh3d(
         x=x_array, y=y_array, z=z_array, alphahull=0, color=color, opacity=opacity
-    ), matrix
+    )
 
 
 def sphere(
     radius=0.5,
-    center=(0., 0., 0.),
-    direction=(0., 0., 1.0),
+    center=(0.0, 0.0, 0.0),
+    direction=(0.0, 0.0, 1.0),
     theta_resolution=30,
     phi_resolution=30,
     start_theta=0.0,
@@ -123,24 +123,23 @@ def sphere(
     end_phi=180.0,
     color="#555",
     opacity=0.5,
-    
 ) -> go.Mesh3d:
     anchor_x, anchor_y, anchor_z = center
-    phi = np.linspace(start_phi, 2*np.radians(end_phi), 2*phi_resolution)
+    phi = np.linspace(start_phi, 2 * np.radians(end_phi), 2 * phi_resolution)
     theta = np.linspace(start_theta, np.radians(end_theta), theta_resolution)
 
     theta, phi = np.meshgrid(theta, phi)
 
-    radius_zy = radius*np.sin(theta)
+    radius_zy = radius * np.sin(theta)
     z_array = np.ravel(anchor_z + np.cos(phi) * radius_zy)
     y_array = np.ravel(anchor_y + np.sin(phi) * radius_zy)
     x_array = np.ravel(anchor_x + radius * np.cos(theta))
 
-    matrix = np.array([x_array, y_array, z_array, np.ones(len(x_array))])
+    x_array, y_array, z_array = apply_transformations(x_array, y_array, z_array, center, direction)
 
     return go.Mesh3d(
         x=x_array, y=y_array, z=z_array, alphahull=0, color=color, opacity=opacity
-    ), matrix
+    )
 
 
 def line(
@@ -149,7 +148,7 @@ def line(
     resolution=1,
     color: str = "#aaa",
     line_width: float = 1.0,
-    opacity: float = 0.8
+    opacity: float = 0.8,
 ):
     """
     Returns a trace of a line in 3d space.
@@ -161,20 +160,27 @@ def line(
     y_array = np.linspace(y0, y1, resolution + 1, endpoint=True)
     z_array = np.linspace(z0, z1, resolution + 1, endpoint=True)
 
-    matrix = np.array([x_array, y_array, z_array, np.ones(len(x_array))])
     line_properties = dict(color=color, width=line_width)
-    return go.Scatter3d(x=x_array, y=y_array, z=z_array, opacity=opacity, mode='lines', line=line_properties), matrix
+    return go.Scatter3d(
+        x=x_array,
+        y=y_array,
+        z=z_array,
+        opacity=opacity,
+        mode="lines",
+        line=line_properties,
+    )
 
 
 def circular_arc_from_normal(
     center=(0.0, 0.0, 0.0),
     resolution=100,
-    normal=[0., 0., 1.],
+    normal=[0.0, 0.0, 1.0],
     angle=90.0,
     polar=[1.0, 0.0, 0.0],
     color="#555",
     opacity=0.5,
     line_width=1.0,
+    return_points=False,
 ) -> go.Mesh3d:
     """
     Create a circular arc defined by normal to the plane of the arc, and an
@@ -212,10 +218,29 @@ def circular_arc_from_normal(
     x_array = np.cos(angles) + radius + anchor_x
     y_array = np.sin(angles) + radius + anchor_y
     z_array = np.zeros(resolution + 1) + anchor_z
-    arc = {'points': np.array([x_array, y_array]).T}
-    line_properties = dict(color=color, width=line_width)
-    return go.Scatter3d(x=x_array, y=y_array, z=z_array, line=line_properties, opacity=opacity, mode='lines'), arc
 
+    x_array, y_array, z_array = apply_transformations(x_array, y_array, z_array, center, normal)
+
+    points = np.array([x_array, y_array]).T
+    line_properties = dict(color=color, width=line_width)
+    if return_points:
+        return go.Scatter3d(
+            x=x_array,
+            y=y_array,
+            z=z_array,
+            line=line_properties,
+            opacity=opacity,
+            mode="lines",
+        ), points
+    else:
+        return go.Scatter3d(
+            x=x_array,
+            y=y_array,
+            z=z_array,
+            line=line_properties,
+            opacity=opacity,
+            mode="lines",
+        )
 
 
 def rectangle(
@@ -226,37 +251,51 @@ def rectangle(
     color: str = "#aaa",
     opacity: float = 0.5,
 ) -> go.Mesh3d:
-    x0 = center[0] - b / 2
-    x1 = center[0] + b / 2
+    z0 = center[2] - b / 2
+    z1 = center[2] + b / 2
 
     y0 = center[1] - d / 2
     y1 = center[1] + d / 2
 
-    z0 = center[2] - z_length / 2
-    z1 = center[2] + z_length / 2
+    x0 = center[0]
+    x1 = center[0]
 
     x_array = [x0, x1, x1, x0, x0, x1, x1, x0]
     y_array = [y0, y0, y1, y1, y0, y0, y1, y1]
     z_array = [z0, z0, z0, z0, z1, z1, z1, z1]
 
-    i_array = [0, 1]
-    j_array = [1, 2]
-    k_array = [3, 3]
+    # i_array = [0, 1]
+    # j_array = [1, 2]
+    # k_array = [3, 3]
+
+    x_array, y_array, z_array = apply_transformations(x_array, y_array, z_array, center, normal)
 
     mesh = go.Mesh3d(
-        x=x_array, y=y_array, z=z_array, opacity=opacity, color=color, alphahull=0
+        x=x_array,
+        y=y_array,
+        z=z_array,
+        opacity=opacity,
+        color=color,
+        alphahull=0,
     )
-    # mesh = go.Mesh3d(
-    #     x=x_array, 
-    #     y=y_array, 
-    #     z=z_array,
-    #     i=i_array,
-    #     j=j_array,
-    #     k=k_array,
-    #     opacity=opacity, 
-    #     color=color,
-    # )
     return mesh
+
+
+def apply_transformations(
+    x_array: nptp.ArrayLike,
+    y_array: nptp.ArrayLike,
+    z_array: nptp.ArrayLike,
+    center: tuple,
+    direction: tuple,
+) -> nptp.ArrayLike:
+    """
+    Re-orients the point arrays to the new 'direction' and translates
+    them to the new 'center'.
+    """
+    point_matrix = build_point_matrix(x_array, y_array, z_array)
+    new_point_matrix = transform_points(point_matrix, center, direction)
+    trans_x_array, trans_y_array, trans_z_array = new_point_matrix
+    return trans_x_array, trans_y_array, trans_z_array
 
 
 def build_point_matrix(
@@ -265,7 +304,7 @@ def build_point_matrix(
     z_array: nptp.ArrayLike,
 ) -> nptp.ArrayLike:
     """
-    Returns a point matrix that can have transforms applied to it from a 
+    Returns a point matrix that can have transforms applied to it from a
     4x4 transformation matrix.
     """
     assert len(x_array) == len(y_array) == len(z_array)
@@ -294,72 +333,66 @@ def transform_points(
     oriented_point_matrix = transform_matrix @ point_matrix
     oriented_point_matrix_3 = oriented_point_matrix[0:3]
     if not np.allclose(new_center, [0.0, 0.0, 0.0]):
-        translated_point_matrix = np.array(
-            new_center, 
-            dtype=point_matrix.dtype
-        ) + oriented_point_matrix_3.T
+        translated_point_matrix = (
+            np.array(new_center, dtype=point_matrix.dtype) + oriented_point_matrix_3.T
+        )
     else:
         translated_point_matrix = oriented_point_matrix_3.T
     return translated_point_matrix.T
 
 
-def rectangular_grid(
-    center=(0.0, 0.0, 0.0),
-    b=1.0,
-    d=1.0,
-    normal=(0., 0., 1.0),
-    rows=1,
-    cols=1,
-    color: str = "#aaa"
-) -> go.Mesh3d:
-    """
-    Returns a grid like:
-     ... ... ...
-    | . | . | . |
-    | 3 | 4 | 5 | ...
-    | 0 | 1 | 2 | ...
+# def rectangular_grid(
+#     center=(0.0, 0.0, 0.0),
+#     b=1.0,
+#     d=1.0,
+#     normal=(0.0, 0.0, 1.0),
+#     rows=1,
+#     cols=1,
+#     color: str = "#aaa",
+# ) -> go.Mesh3d:
+#     """
+#     Returns a grid like:
+#      ... ... ...
+#     | . | . | . |
+#     | 3 | 4 | 5 | ...
+#     | 0 | 1 | 2 | ...
 
-    Where 0, 1, 2, 3, 4, ... etc. are the "indexes" of the grid
-    rectangles. They are numbered from the bottom-left left-to-right,
-    down-to-up until the bth rectangle which has an index of (m * n - 1)
-    where m is rows and n is columns.
+#     Where 0, 1, 2, 3, 4, ... etc. are the "indexes" of the grid
+#     rectangles. They are numbered from the bottom-left left-to-right,
+#     down-to-up until the bth rectangle which has an index of (m * n - 1)
+#     where m is rows and n is columns.
 
-    b: total width of grid
-    d: total depth (height) of grid
+#     b: total width of grid
+#     d: total depth (height) of grid
 
-    color: str | dict[Callable, str] will color the rectangles either all
-        one color (str) or conditionally color them based on whether the
-        index value of each rectangle returns True in the dict callable key
-        (the color in the value will be applied if True; the first matching
-        condition applies).
+#     color: str | dict[Callable, str] will color the rectangles either all
+#         one color (str) or conditionally color them based on whether the
+#         index value of each rectangle returns True in the dict callable key
+#         (the color in the value will be applied if True; the first matching
+#         condition applies).
 
-    """
-    # nodes
-    center = np.array(center)
-    normal = np.array(normal)
+#     """
+#     # nodes
+#     center = np.array(center)
+#     normal = np.array(normal)
 
-    # Direction cosines
-    x_dir = (1, 0, 0)
-    y_dir = (0, 1, 0)
-    z_dir = (0, 0, 1)
-    assumed_normal = z_dir
-    norm = np.linalg.norm(normal)
-    cos_alpha = np.dot(x_dir, normal) / norm
-    cos_beta = np.dot(y_dir, normal) / norm
-    cos_gamma = np.dot(z_dir, normal) / norm
+#     # Direction cosines
+#     x_dir = (1, 0, 0)
+#     y_dir = (0, 1, 0)
+#     z_dir = (0, 0, 1)
+#     assumed_normal = z_dir
+#     norm = np.linalg.norm(normal)
+#     cos_alpha = np.dot(x_dir, normal) / norm
+#     cos_beta = np.dot(y_dir, normal) / norm
+#     cos_gamma = np.dot(z_dir, normal) / norm
 
-    pitch_rot = np.array([
-        [cos_beta, 0, 1-cos_beta],
-        [0, 1, 0],
-        [-1-cos_beta, 0, cos_beta]
-    ])
+#     pitch_rot = np.array(
+#         [[cos_beta, 0, 1 - cos_beta], [0, 1, 0], [-1 - cos_beta, 0, cos_beta]]
+#     )
 
-    yaw_rot = np.array([
-        [cos_gamma, -1-cos_gamma, 0],
-        [1-cos_gamma, cos_gamma, 0],
-        [0, 0, 1]
-    ])
-
+#     yaw_rot = np.array(
+#         [[cos_gamma, -1 - cos_gamma, 0], [1 - cos_gamma, cos_gamma, 0], [0, 0, 1]]
+#     )
 
     # # The projection of the normal on each plane
     # xy_proj = np.array([normal[0], normal[1], 0])
@@ -372,7 +405,7 @@ def rectangular_grid(
 
     # # Go "down" to the mid-point of the bottom edge of the rectangle
     # bot_mid_point = (
-    #     center 
+    #     center
     #     - (
     #         yz_perp / np.linalg.norm(yz_perp) if np.sum(yz_perp) != 0 else np.zeros(3)
     #         + xz_perp / np.linalg.norm(xz_perp) if np.sum(xz_perp) != 0 else np.zeros(3)
@@ -383,7 +416,7 @@ def rectangular_grid(
 
     # # Go "up" to the mid-poitn of the top edge of the rectangle
     # top_mid_point = (
-    #     center 
+    #     center
     #     + (
     #         yz_perp / np.linalg.norm(yz_perp) if np.sum(yz_perp) != 0 else np.zeros(3)
     #         + xz_perp / np.linalg.norm(xz_perp) if np.sum(xz_perp) != 0 else np.zeros(3)
@@ -396,7 +429,6 @@ def rectangular_grid(
 
     # print(hypot_length)
     # print(np.linalg.norm(B_point - A_point))
-
 
     # Plane equation
     "normal[0] * x + normal[1] * y + normal[2] * z = np.dot(center, normal)"
@@ -411,7 +443,6 @@ def rectangular_grid(
     # Therefore vector of the bottom edge will be the -ve reciprocal of the xy projection of the vector normal [1, 2, 3] => [1, 2, 0]
     # So the bottom edge will be [-2, 1, 0]
 
-
     # triangles
     for j in range(rows):
         mod_co = cols + 1
@@ -422,6 +453,7 @@ def rectangular_grid(
 
             tri_1 = [anchor_node, anchor_node + mod_ro, anchor_node + mod_ro + 1]
             tri_2 = [anchor_node, anchor_node + 1, anchor_node + mod_ro + 1]
+
 
 # From Pyvista
 def reorient(direction=(1.0, 0.0, 0.0)):
@@ -455,5 +487,3 @@ def reorient(direction=(1.0, 0.0, 0.0)):
     trans[3, 3] = 1
 
     return trans
-
-
--- tests/test_shapes.py
+++ tests/test_shapes.py
@@ -2,6 +2,7 @@ import numpy as np
 import numpy.testing as npt
 from plotly_3d_primitives import shapes
 
+
 def test_build_point_matrix():
     x_array = np.array([0, 0])
     y_array = np.array([0, 1])
@@ -10,13 +11,16 @@ def test_build_point_matrix():
 
     npt.assert_array_equal(
         matrix,
-        np.array([
-            [0, 0],
-            [0, 1],
-            [0, 0],
-            [1, 1],
-            ])
+        np.array(
+            [
+                [0, 0],
+                [0, 1],
+                [0, 0],
+                [1, 1],
+            ]
+        ),
     )
+
 
 def test_transform_points():
     x_array = np.array([0, 1])
@@ -27,19 +31,23 @@ def test_transform_points():
 
     npt.assert_array_almost_equal(
         new_matrix_1,
-        np.array([
-            [0, 0],
-            [0, 1],
-            [0, 0],
-            ])
+        np.array(
+            [
+                [0, 0],
+                [0, 1],
+                [0, 0],
+            ]
+        ),
     )
 
     new_matrix_2 = shapes.transform_points(matrix, (10, 10, 10), (0, 1, 0))
     npt.assert_array_almost_equal(
         new_matrix_2,
-        np.array([
-            [10, 10],
-            [10, 11],
-            [10, 10],
-            ])
+        np.array(
+            [
+                [10, 10],
+                [10, 11],
+                [10, 10],
+            ]
+        ),
     )
